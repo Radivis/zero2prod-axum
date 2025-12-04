368491b91312eabc3940d050749c0ef2463f469f
Radivis <radivis@radivis.com>
2025-12-04 12:02:31 +0100

    Added config for Docker

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,13 @@ async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let host = configuration.application.host;
+    let address = format!("{}:{}", host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     let post_binding_error_message =
-        format!("Failed to bind to port {}", configuration.application_port);
+        format!("Failed to bind to port {}", configuration.application.port);
     run(listener, connection_pool)
         .expect(&post_binding_error_message)
         .await
